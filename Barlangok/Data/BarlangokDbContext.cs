bc71eb2f7d11ad4caeac97d9b0327168ab12cf7c
Barna István <barna.istvan@burg.lo.hu>
2025-09-30 08:44:21 +0200

    1-2 Feladat

---
+++ Barlangok/Data/BarlangokDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Barlangok.Models;
+
+namespace Barlangok.Data
+{
+    public class BarlangokDbContext : DbContext
+    {
+        public BarlangokDbContext(DbContextOptions<BarlangokDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Barlangok.Models.BarlangokModel> Barlangok { get; set; }
+    }
+}
--- Barlangok/Program.cs
+++ Barlangok/Program.cs
@@ -1,8 +1,12 @@
+using Barlangok.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddDbContext<BarlangokDbContext>(
+    options => options.UseSqlite(builder.Configuration.GetConnectionString("BarlongokKapcsolatString")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
